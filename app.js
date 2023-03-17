a2b3495d26f9bd7c087b523661cd2aa7d44dd13b
Paula Siano <Paula_Siano@mckinsey.com>
2023-03-17 18:00:42 -0300

    refactor: refactored acccording to styles

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 /* Middlewares*/
-if(process.env.NODE_ENV === 'development'){
+if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
