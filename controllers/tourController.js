88eafa147d0af4e0b32db0d94bda138232cd8974
Paula Siano <Paula_Siano@mckinsey.com>
2023-03-21 15:37:23 -0300

    feat: added sorting

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -10,14 +10,21 @@ exports.getAllTours = async (req, res) => {
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    //2-) Advanced Filtering
+    // Advanced Filtering
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
     console.log(JSON.parse(queryStr));
 
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
 
-    // { difficulty : 'easy'}
+    //Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      console.log(sortBy);
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
 
     //Execute query
     const tours = await query;
