1d1d031f2b03c7ce19d8c5a0559f0eb6952b3b32
Paula Siano <Paula_Siano@mckinsey.com>
2023-03-21 16:15:21 -0300

    fix: erased unnecessary property

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ mongoose
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
-    useUnifiedTopology: true,
   })
   .then(() => console.log('DB connected successfully'));
 
