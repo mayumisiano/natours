98167d803bd6492118a9757c812d51c96916236c
Paula Siano <Paula_Siano@mckinsey.com>
2023-03-21 11:35:22 -0300

    feat: added new fields to tourSchema

--- models/tourModel.js
+++ models/tourModel.js
@@ -7,14 +7,50 @@ const tourSchema = new mongoose.Schema({
     required: [true, 'A tour must have a name'],
     unique: true,
   },
-  rating: {
+  durations: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a maxGroupSize'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsAverage: {
     type: Number,
     required: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    required: [true, 'A tour must have a ratingsQuantity'],
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a summary'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have an imageCover'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
