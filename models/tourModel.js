4f546a384f9cbae4e799ec6df42083b4bbb2f986
Paula Siano <Paula_Siano@mckinsey.com>
2023-03-22 16:33:41 -0300

    feat: created Aggregation middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -56,6 +56,10 @@ const tourSchema = new mongoose.Schema(
       select: false,
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: { virtuals: true },
@@ -82,6 +86,25 @@ tourSchema.pre('save', function (next) {
 //   console.log(doc);
 //   next();
 // });
+
+//Query Middleware
+tourSchema.pre(/ˆfind/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/ˆfind/, (docs, next) => {
+  console.log(`Query took ${Date.now() - this.start} miliseconds!`);
+  console.log(docs);
+  next();
+});
+
+tourSchema.pre('findOne', function (next) {
+  this.find({ secretTour: { $ne: true } });
+  next();
+});
 
 const Tour = mongoose.model('Tour', tourSchema);
 
