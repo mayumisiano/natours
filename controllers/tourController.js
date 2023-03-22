2f9a16ab8d8dd2d36ba75b5fd3993b5a92653f09
Paula Siano <Paula_Siano@mckinsey.com>
2023-03-22 11:30:13 -0300

    feat: created Aliasing function/route

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,13 @@
 // eslint-disable-next-line import/no-useless-path-segments, no-unused-vars
 const Tour = require('./../models/tourModel');
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
+
 /* Get All Tours */
 exports.getAllTours = async (req, res) => {
   try {
