0f6b7e1e71c721202f9dfa498c8f8ce99ec48b05
Paula Siano <Paula_Siano@mckinsey.com>
2023-03-24 16:46:52 -0300

    feat: handling duplicate database fields

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -7,6 +7,12 @@ const handleCastErrorDB = (error) => {
   return new AppError(message, 400);
 };
 
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  const message = `Duplicate field value entered: ${value}. Please enter another value!`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -42,7 +48,9 @@ module.exports = (err, req, res, next) => {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
     let error = { ...err };
-    if (error.name === 'CastError') err = handleCastErrorDB(err);
+
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
 
     sendErrorProd(error, res);
   }
