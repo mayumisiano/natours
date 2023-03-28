07a1ee4f592f45a7ca6dd2dc8d050dfef9543db4
Paula Siano <Paula_Siano@mckinsey.com>
2023-03-28 16:31:54 -0300

    feat: JWT authentication, signing up/logging Users

--- models/userModel.js
+++ models/userModel.js
@@ -19,6 +19,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Password is required'],
     minLength: 8,
+    select: false,
   },
   passwordConfirm: {
     type: String,
@@ -39,6 +40,13 @@ userSchema.pre('save', async function (next) {
 
   this.passwordConfirm = undefined;
 });
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model('User', userSchema);
 
