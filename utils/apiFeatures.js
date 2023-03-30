d62fb9127368566d051643a8d39a721a21b30e33
Paula Siano <Paula_Siano@mckinsey.com>
2023-03-30 17:28:16 -0300

    feat: preventing parameter pollution

--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -21,6 +21,7 @@ class APIFeatures {
 
   sort() {
     if (this.queryString.sort) {
+      
       const sortBy = this.queryString.sort.split(',').join(' ');
       this.query = this.query.sort(sortBy);
     } else {
